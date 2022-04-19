34aeffa833569ec4a83422d8f735c182c0a52d94
Peter <41585899+pbanigo@users.noreply.github.com>
2022-04-19 19:47:27 +0100

    Fixed remove button issue

--- js/main.js
+++ js/main.js
@@ -34,20 +34,22 @@ function updateCardIds(){
 //create the function that toggles a book’s read status on your Book prototype instance.
 
 
-let idNum = 0;
+
 let myLibrary = JSON.parse(localStorage.getItem("allEntries"));//Pick all items from localStorage
 const container = document.querySelector('#books');//Assign our book container to it's div
 if(myLibrary == null) {//if item parsed from local storage is empty
     myLibrary = []; //assign library to an empty array
+    let idNum = 0;// Initialize IdNum
 } else {
     //display all items in library on cards
+    let idNum = Number(localStorage.getItem("idNum"));
     displayBooks();
 }
 //Write a function that loops through the array and displays each book on the page. #You can display them in some sort of table, or each on their own “card”. 
 function displayBooks(){
     for (let i = 0; i < myLibrary.length; i++) {
-        let item = myLibrary[i];
-        createCard(item.title,item.author,item.pages)// run the create card function for each object in my Library
+        let item = myLibrary[i]; //.idNo should give number to use?
+        createCard(item.idNo,item.title,item.author,item.pages)// run the create card function for each object in my Library
     }
 }
 function countBooks(){
@@ -56,20 +58,21 @@ function countBooks(){
 countBooks()//Show number of books in library on load
 
 class Book {
-    constructor(title, author, pages){
+    constructor(idNo, title, author, pages){
+        this.idNo = idNo
         this.title = title
         this.author = author
         this.pages = pages
         //this.read = read
     }
 }
-function createCard (title,author, pages){//function to create a card div
+function createCard (idNo,title,author, pages){//function to create a card div
     const card = document.createElement('div');
     card.classList.add("card");
     //One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-    card.id = `data-${idNum}`;
+    card.id = `data-${idNo}`;
     //You will need to associate your DOM elements with the actual book objects in some way. 
-    const cardId = idNum;
+    const cardId = idNo;
     const p1 = document.createElement("p");
     p1.textContent = `Title is ${title}`;
     const p2 = document.createElement("p");
@@ -88,12 +91,14 @@ function createCard (title,author, pages){//function to create a card div
        // updateCardIds(); //
         document.getElementById(`data-${cardId}`).remove(); 
         console.log(`${myLibrary.length} after card ${cardId} removed`);
-        myLibrary.splice(cardId, 1); 
+        myLibrary = myLibrary.filter(function(el) { return el.idNo != cardId; }); 
+       // myLibrary.splice(cardId, 1); //removes the wrong thing
         console.log(`${myLibrary.length} after card ${cardId} splice`);
     if(myLibrary.length > 0) {//if anything in library, save in localStorage
         localStorage.setItem("allEntries", JSON.stringify(myLibrary)); 
 } else {//else delete the library from localStorage
     localStorage.removeItem("allEntries");
+    localStorage.removeItem("idNum");
 }
 countBooks();
 return false;
@@ -104,13 +109,16 @@ card.appendChild(p2);
 card.appendChild(checkBox);
 card.appendChild(button);
 container.appendChild(card);
-idNum += 1;
+//idNum += 1;
 
 }
 
 
 //add a function to the script  that can take user’s input and store the new book objects into an array. 
 function addBookToLibrary() {
+
+    let idNum = Number(localStorage.getItem("idNum"));  
+    idNum+=1;
     let bookTitle = document.getElementById('title').value;
     let bookAuthor = document.getElementById('author').value;
     let bookPages = document.getElementById('pages').value;
@@ -118,10 +126,11 @@ function addBookToLibrary() {
         alert("You need to fill in all fields!")
     } else {
 
-        const newBook = new Book(bookTitle,bookAuthor,bookPages);
+        const newBook = new Book(idNum,bookTitle,bookAuthor,bookPages);
         myLibrary.push(newBook);
         localStorage.setItem("allEntries", JSON.stringify(myLibrary));
-        let card = createCard(bookTitle,bookAuthor,bookPages);
+        localStorage.setItem("idNum", idNum);
+        let card = createCard(idNum,bookTitle,bookAuthor,bookPages);
     }
     countBooks();//update number after adding new book
 
